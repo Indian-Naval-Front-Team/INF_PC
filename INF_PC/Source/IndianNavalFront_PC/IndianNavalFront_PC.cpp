7b2459017e4b0aed27c22a5ca78e9557c0b328b5
Neosphere Interactive <71254707+neosphereinteractive-real@users.noreply.github.com>
2021-01-04 19:11:11 +0530

    Deleting the old project before recreating a new one.

--- INF_PC/Source/IndianNavalFront_PC/IndianNavalFront_PC.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "IndianNavalFront_PC.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IndianNavalFront_PC, "IndianNavalFront_PC" );
